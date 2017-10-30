2945764b379372925caba9b5473261ecac3d90ce
moandy <971058171@qq.com>
2017-10-30 10:33:06 +0800

    leetcode

--- leetcode416.cpp
+++ leetcode416.cpp
@@ -6,26 +6,40 @@
 #include <algorithm>
 using namespace std;
 
+// class Solution {
+// public:
+//     bool flag = false;
+//     bool canPartition(vector<int>& nums) {
+//         int target = accumulate(nums.begin(), nums.end(), 0);
+//         if (target % 2 != 0) return false;
+//         isok(0, nums, target/2);
+//         return flag;
+//     }
+//
+//     void isok(int i, vector<int>& nums, int target) {
+//         if (target == 0&&i <= nums.size()) {
+//             flag = true;
+//         }
+//         if (i >= nums.size()) return;
+//         target -= nums[i];
+//         isok(i+1, nums,target);
+//         target += nums[i];
+//         isok(i+1, nums,target);
+//     }
+// };
+
 class Solution {
 public:
-    bool flag = false;
     bool canPartition(vector<int>& nums) {
-        int target = accumulate(nums.begin(), nums.end(), 0);
-        if (target % 2 != 0) return false;
-        isok(0, nums, target/2);
-        return flag;
-    }
-
-    void isok(int i, vector<int>& nums, int target) {
-        if (target == 0&&i <= nums.size()) {
-            flag = true;
-        }
-        if (i >= nums.size()) return;
-        target -= nums[i];
-        isok(i+1, nums,target);
-        target += nums[i];
-        isok(i+1, nums,target);
-    }
+    int sum = accumulate(nums.begin(), nums.end(), 0), target = sum >> 1;
+    if (sum & 1) return false;
+    vector<int> dp(target + 1, 0);
+    dp[0] = 1;
+    for(auto num : nums)
+        for(int i = target; i >= num; i--)
+            dp[i] = dp[i] || dp[i - num];
+    return dp[target];
+}
 };
 
 int main(int argc, char const *argv[]) {
